9f5c19f75d036be07477a817e6790e16dfe17f23
Kate <kate.stark13@sbcglobal.net>
2018-02-15 18:11:35 -0600

    updates to apiRoutes.js

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,65 @@
+//require friends data file
+var friends = require('../data/friends.js');
+
+
+
+//routes
+module.exports = function(app){
+	//API get requests
+	app.get('/api/friends', function(req, res){
+		res.json(friends);
+	});
+	//API POST requests
+	app.post('/api/friends', function(req, res){
+		//object to hold the best match
+		var bestMatch = {
+			name: "",
+			photo: "",
+			friendDifferent: 1000
+		};
+
+		//use the result of the users survey POST and parse
+		var userData = req.body;
+		var userName = userData.name;
+		var userPhoto = userData.photo;
+		var userScores = userData.scores;
+
+		var totalDifference = 0;
+
+		//loop through all the friend possibilities in the database
+		for(var i = 0; i < friends.length; i++) {
+			console.log(friends[i].name);
+			totalDifference = 0;
+
+			//loop through all the scores for each friend
+			for(var j = 0; j < friends[i].scores[j]; j++) {
+
+				//calculate the different between the scores and sum into the totalDifference
+				totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
+				
+				//if sum of difference is less than difference of current best match
+				if (totalDifference <= bastMatch.friendDifference) {
+
+					//reset the bestMatch to be the new friend
+					bestMatch.name = friends[i].name;
+					bestMatch.photo = friends[i].photo;
+					bestMatch.friendDifference = totalDifference;
+				}
+			}
+		}
+		//save users data to the database
+		friends.push(userData);
+
+		//return a JSON with users bestMatch.  
+		res.json(bestMatch);
+	});
+
+}
+
+app.get("/", function(req, res){
+	res.sendFile(path.join(__dirname, "app/public/home.html"))
+});
+
+app.get("/survey", function(req, res){
+	res.sendFile(path.join(__dirname, "app/public/survey.html"))
+});
---
+++ server.js
@@ -0,0 +1,23 @@
+//set up dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+//create express server and sets up port
+var app = express();
+var PORT = /*process.eng.PORT || */3000;
+
+//set up express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+
+//router
+require('app/routing/apiRoutes.js')(app);
+require('app/routing/htmlRoutes.js')(app);
+
+
+//starts the server to begin listening
+app.listen(PORT, function(){
+	console.log("App listening on PORT " + PORT);
+});
